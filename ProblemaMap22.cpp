57baccad96ff1d6496641d819c11cc9440f7aeeb
georgianafati <georgianafati@gmail.com>
2024-01-09 20:52:20 +0200

    Problema-22

---
+++ ProblemaMap22.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int N = 8;
+
+void afiseazaTabla() {
+}
+
+bool esteSigur() {  
+}
+
+void rezolvaProblemaReginelor() {  
+}
+
+int main() {
+    
+    return 0;
+}
